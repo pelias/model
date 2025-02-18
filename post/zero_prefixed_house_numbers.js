a9ea95fa832fb36df4369981dbd58b77778424ea
Peter Johnson <insomnia@rcpt.at>
2025-02-18 15:37:15 +0100

    feat(post): update zero_prefixed_house_numbers script

--- post/zero_prefixed_house_numbers.js
+++ post/zero_prefixed_house_numbers.js
@@ -22,7 +22,39 @@ function housenumber(doc) {
   if ( houseno.length < 2 || !houseno.startsWith('0') ) { return; }
 
   // trim leading zeros
-  doc.setAddress('number', houseno.replace(/^0+/, ''));
+  const replacement = houseno.replace(/^0+/, '') || '0';
+
+  // update housenumber
+  doc.setAddress('number', replacement);
+
+  //  update address 'name' fields
+  names(doc, houseno, replacement);
+}
+
+function names(doc, houseno, replacement) {
+  // only records on the address layer have the housenumber in the name.
+  if( doc.getLayer() !== 'address' ){ return; }
+
+  // ensure street is set
+  let street = doc.getAddress('street');
+  if( !_.isString(street) || _.isEmpty(street) ){ return; }
+
+  // load name data. ie: an object keyed by language codes, each value is an array of names
+  if (!_.isPlainObject(doc.name)) { return; }
+
+  // $houseno must be surrounded by whitespace or start/end of text
+  const regexp = new RegExp('\\b' + _.escapeRegExp(houseno) + '\\b');
+
+  // iterate over all languages
+  _.each(doc.name, (names, lang) => {
+    if (_.isEmpty(names)) { return; }
+
+    // replace house number in name fields
+    const replaced = _.castArray(names).map(name => name.replace(regexp, replacement));
+
+    // update lang
+    doc.name[lang] = _.size(replaced) === 1 ? _.first(replaced) : replaced;
+  });
 }
 
 module.exports = housenumber;
